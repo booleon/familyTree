8afcd03f7d3ee8460d9954113438aa18a0d5fc98
Pierre_Vanneste <pierrevanneste2012@gmail.com>
2017-04-22 15:42:22 +0200

    test
    test bon papa bonne maman

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>FamilyTree</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+      <!--- Début arbre -->
+      <div class="genealogie">
+        <ul>
+          <li><a href="#">Bon Papa</a></li>
+          <li><a href="#">Bonne Maman</a></li>
+        </ul>
+      </div>
+    <!-- Fin arbre -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
