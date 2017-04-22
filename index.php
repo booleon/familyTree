62707fe35831d51357908fe5e717d64a39e5dbf3
Pierre_Vanneste <pierrevanneste2012@gmail.com>
2017-04-22 17:41:47 +0200

    Début de la structure
    Probleme sur le premier bloc

--- index.php
+++ index.php
@@ -10,14 +10,46 @@
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
+    <div class="container-fluid">
       <!--- Début arbre -->
+
       <div class="genealogie">
         <ul>
-          <li><a href="#">Bon Papa</a></li>
-          <li><a href="#">Bonne Maman</a></li>
+          <!-- ####### DEBUT BON PAPA / BONNE MAMAN ####### -->
+          <li><span class="couple"><a href="#">Bon Papa</a> <a href="#">Bonne Maman</a></span>
+            <ul>
+              <li><a href="#">Rolande</a>
+                <ul>
+                  <li><a href="#">Aline</a>
+                    <ul>
+                      <li><a href="#">Leopaul</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Xavier</a></li>
+                  <li><a href="#">Pascal</a></li>
+                </ul>
+              </li>
+
+              <li><a href="#">Marie-Christine</a></li>
+              <li><a href="#">Colette</a></li>
+              <li><a href="#">Hubert</a></li>
+              <li><a href="#">Cecile</a></li>
+              <li><a href="#">Frederic</a></li>
+              <li><a href="#">Marc</a></li>
+              <li><a href="#">Remy</a></li>
+              <li><a href="#">Véronique</a></li>
+              <li><a href="#">Denis</a></li>
+              <li><a href="#">Bruno</a></li>
+            </ul>
+          </li>
+          <!-- ####### FIN BON PAPA / BONNE MAMAN ####### -->
         </ul>
       </div>
     <!-- Fin arbre -->
+    </div>
+
+
+    <!-- JAVASCRIPT Bootstrap -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </body>
 </html>
