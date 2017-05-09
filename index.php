ac35d6059c4c918936120326323f1154886bf0a4
Pierre_Vanneste <pierrevanneste2012@gmail.com>
2017-05-09 15:45:54 +0200

    MAJ
    J'ai mis toute la descendence direct
    Je pense qu'il va plutot falloir faire cet arbre généalogique
    verticalement ce sera plus lisible car horizontalement ça demande de
    faire défiller de gauche à droite.
    A réfléchir

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-    <div class="container-fluid">
+    <div class="container page">
       <!--- Début arbre -->
+      <h1>GENEALOGIE</h1>
 
       <div class="genealogie">
         <ul>
           <!-- ####### DEBUT BON PAPA / BONNE MAMAN ####### -->
-          <li><span class="couple"><a href="#">Bon Papa</a> <a href="#">Bonne Maman</a></span>
+          <li><span class="couple"><a href="#">Roland</a> <a href="#">Eveline</a></span>
             <ul>
               <li><a href="#">Rolande</a>
                 <ul>
@@ -25,21 +26,69 @@
                       <li><a href="#">Leopaul</a></li>
                     </ul>
                   </li>
-                  <li><a href="#">Xavier</a></li>
-                  <li><a href="#">Pascal</a></li>
+                  <li><a href="#">Xavier</a>
+                    <ul>
+                      <li><a href="#">Mélanie</a><a href="#">Amandine</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Pascal</a>
+                    <ul>
+                      <li><a href="#">Hélios</a></li>
+                    </ul>
+                  </li>
                 </ul>
               </li>
 
-              <li><a href="#">Marie-Christine</a></li>
-              <li><a href="#">Colette</a></li>
-              <li><a href="#">Hubert</a></li>
-              <li><a href="#">Cecile</a></li>
-              <li><a href="#">Frederic</a></li>
-              <li><a href="#">Marc</a></li>
-              <li><a href="#">Remy</a></li>
-              <li><a href="#">Véronique</a></li>
-              <li><a href="#">Denis</a></li>
-              <li><a href="#">Bruno</a></li>
+              <li><a href="#">Marie-Christine</a>
+                <ul>
+                  <li><a href="#">Emmanuel</a><a href="#">Philippe</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Colette</a>
+                <ul>
+                  <li><a href="#">Sylvie</a><a href="#">Yves</a><a href="#">Véronique</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Hubert</a>
+                <ul>
+                  <li><a href="#">Odile</a><a href="#">Richard</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Cecile</a>
+                <ul>
+                  <li><a href="#">Agnès</a><a href="#">Olivier</a><a href="#">Béatrice</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Frederic</a>
+                <ul>
+                  <li><a href="#">Florent</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Marc</a>
+                <ul>
+                  <li><a href="#">Sébastien</a><a href="#">Jérome</a><a href="#">Virginie</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Remy</a>
+                <ul>
+                  <li><a href="#">Stéphanie</a><a href="#">Jérémie</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Véronique</a>
+                <ul>
+                  <li><a href="#">Agathe</a><a href="#">Pierre</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Denis</a>
+                <ul>
+                  <li><a href="#">Marius</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Bruno</a>
+                <ul>
+                  <li><a href="#">Carole</a><a href="#">Marie-Claire</a><a href="#">Laura</a></li>
+                </ul>
+              </li>
             </ul>
           </li>
           <!-- ####### FIN BON PAPA / BONNE MAMAN ####### -->
